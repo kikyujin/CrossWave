893cbabe9f196274a08afbd194793fba70ad1be6
kikyujin <contact@kikyujin.com>
2026-03-07 08:20:22 +0900

    Initial Commit

---
+++ CrossWave/CrossWaveApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrossWaveApp.swift
+//  CrossWave
+//
+//  Created by Mahito KIDA on 2026/03/07.
+//
+
+import SwiftUI
+
+@main
+struct CrossWaveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
